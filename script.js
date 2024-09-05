0c3cbbf06352b84e43ff83d63c0558b11976c324
Haj-Hadi <147650376+Haj-Hadi@users.noreply.github.com>
2024-09-05 16:42:38 +0330

    Update script.js

--- script.js
+++ script.js
@@ -52,22 +52,22 @@ document.addEventListener('DOMContentLoaded', () => {
             promoId: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
             eventsDelay: 21000,
             attemptsNumber: 22,
-	    },
-	    8: {
+	},
+	8: {
             name: 'Fluff Crusade',
             appToken: '112887b0-a8af-4eb2-ac63-d82df78283d9',
             promoId: '112887b0-a8af-4eb2-ac63-d82df78283d9',
-            eventsDelay: 23000,
-            attemptsNumber: 18,
+            eventsDelay: 20000,
+            attemptsNumber: 32,
         },
-    	9: {
+	9: {
             name: 'Tile Trio',
             appToken: 'e68b39d2-4880-4a31-b3aa-0393e7df10c7',
             promoId: 'e68b39d2-4880-4a31-b3aa-0393e7df10c7',
-            eventsDelay: 40000,
+            eventsDelay: 20000,
             attemptsNumber: 22,
             
-	    }
+	}
     };
 
     // Morse code map for digits
